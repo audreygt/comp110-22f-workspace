eeaec822656c2a68ac0b47a8dc2a359e2f7ec640
audreygt <audreyt@unc.edu>
2022-08-19 13:32:26 -0400

    just messing around

--- lessons/input.py
+++ lessons/input.py
@@ -1,7 +1,7 @@
 """Demonstrates asking the user for input."""
 
 user_name: str = input("What is your name?" )
-print("Hello," + user_name + ", good morning!")
+print("Hello, " + user_name + ", good morning!")
 print("You are awesome, " + user_name)
-print("At 9 AM", + user_name + ".")
+print("At 9 AM, " + user_name + ",you are going to make a new Spotify playlist.")
 
