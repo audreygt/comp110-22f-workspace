c7046ca50725645b15170330a94b5da286c7e833
audreygt <audreyt@unc.edu>
2022-08-20 00:48:43 -0400

    im done editing for the night

--- lessons/input.py
+++ lessons/input.py
@@ -3,6 +3,6 @@
 user_name: str = input("What is your name?" )
 print("Hello, " + user_name + ", good morning!")
 print("You are awesome, " + user_name)
-print("At 9 AM, " + user_name + ", you are going to make a new Spotify playlist.")
-print("Your sister, " + user_name + ", will be spending time with you tomorrow.")
 print(user_name + ", you need to put your teeth up.")
+print(user_name + ", your family is full of clowns.")
+print("You go to sleep too early, " + user_name)
