75802b113a47a3e0170166c01645a2fb1684f941
audreygt <audreyt@unc.edu>
2022-08-20 00:38:05 -0400

    Playing around with my coding

--- lessons/input.py
+++ lessons/input.py
@@ -3,5 +3,6 @@
 user_name: str = input("What is your name?" )
 print("Hello, " + user_name + ", good morning!")
 print("You are awesome, " + user_name)
-print("At 9 AM, " + user_name + ",you are going to make a new Spotify playlist.")
-
+print("At 9 AM, " + user_name + ", you are going to make a new Spotify playlist.")
+print("Your sister, " + user_name + ", will be spending time with you tomorrow.")
+print(user_name + ", you need to put your teeth up.")
