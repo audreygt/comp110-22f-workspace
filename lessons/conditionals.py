100b52733e22992c94ca72dc9ce07832aaf46b2b
audreygt <audreyt@unc.edu>
2022-08-22 15:29:16 -0400

    finished conditional statements

---
+++ lessons/conditionals.py
@@ -0,0 +1,19 @@
+"""An example of conditional (if-else) statements."""
+
+SECRET: int = 3
+
+print("I'm thinking of a number 1-5, what is it?")
+guess: int = (int(input("What is your guess? ")))
+
+if guess == SECRET: 
+    print("You guessed correctly!!!")
+    print("You did great, have a wonderful day!!!")
+else:
+    print("Sorry, you guess incorrectly :(")
+    if guess > SECRET:
+        print("Try a lower number.")
+    else:
+        print("Try a higher number.")
+
+print("Game over.")
+print("Audrey may switch her major if these FAFSA people make her lose her mind.")
