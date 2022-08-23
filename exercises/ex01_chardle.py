17274960d4e1b34be259bfe6bddf0d86897a8a36
audreygt <audreyt@unc.edu>
2022-08-23 17:42:35 -0400

    im tired of doing this

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -5,5 +5,3 @@ __author__ = "730545277"
 character_word: str = input("Enter a 5-character word: ")
 single_character: str = input("Enter a single character: ")
 print("Searching for " + single_character + " in " + character_word)
-if single_character == str(character_word[single_character])
-
