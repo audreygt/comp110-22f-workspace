373a3793816a4ceef262e11042bc895dbc906a25
audreygt <audreyt@unc.edu>
2022-08-24 10:14:12 -0400

    still messing around

--- exercises/ex01_chardle3.py
+++ exercises/ex01_chardle3.py
@@ -4,32 +4,34 @@ __author__ = "730545277"
 
 from operator import countOf
 
-
 typed_word: str = input("Enter a 5-character word: ")
 LIMIT: int = 5
-if len(typed_word) >= LIMIT:
+if len(typed_word) > LIMIT:
     print("Error: Word must contain 5 characters")
-
-typed_character: str = input("Enter a single character: ")
-print("searching for " + typed_character + " in " + typed_word)
-LIMIT: int = 1
-if len(typed_character) >= LIMIT:
-    print("Error: Character must be a single character.")
-
-if typed_word[0] == typed_character:
-    print(typed_character + " found at index 0")
+    quit()
 else:
-    if typed_word[1] == typed_character: 
-        print(typed_character + " found at index 1")
+    typed_character: str = input("Enter a single character: ")
+    print("searching for " + typed_character + " in " + typed_word)
+
+LIMIT: int = 1
+if len(typed_character) > LIMIT:
+    print("Error: Character must be a single character.")
+    quit()
+else:
+    if typed_word[0] == typed_character:
+        print(typed_character + " found at index 0")
     else:
-        if typed_word[2] == typed_character:
-            print(typed_character + " found in index 2")
-        else: 
-            if typed_word[3] == typed_character:
-                print(typed_character + " found in index 3")
-            else:
-                if typed_word[4] == typed_character:
-                    print(typed_character + " found in index 4")
+        if typed_word[1] == typed_character: 
+         print(typed_character + " found at index 1")
+        else:
+            if typed_word[2] == typed_character:
+             print(typed_character + " found in index 2")
+            else: 
+                if typed_word[3] == typed_character:
+                    print(typed_character + " found in index 3")
+                else:
+                    if typed_word[4] == typed_character:
+                     print(typed_character + " found in index 4")
 
 MATCH: int = 1
 if (countOf[typed_character]) == MATCH:
