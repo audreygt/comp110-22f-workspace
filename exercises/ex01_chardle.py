cde1eb85a333124ba424d631944e3002a2ea7aa7
audreygt <audreyt@unc.edu>
2022-08-25 22:32:38 -0400

    final chardle maybe

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,7 +1,47 @@
 """EX01 - Chardle - A cute step toward Wordle."""
-
 __author__ = "730545277"
 
-character_word: str = input("Enter a 5-character word: ")
-single_character: str = input("Enter a single character: ")
-print("Searching for " + single_character + " in " + character_word)
+number: int = 0
+typed_word: str = input("Enter a 5-character word: ")
+if len(typed_word) > 5:
+    print("Error: Word must contain 5 characters")
+    exit()
+typed_character: str = input("Enter a single character: ")
+print("Searching for " + typed_character + " in " + typed_word)
+if len(typed_character) > 1:
+    print("Error: Character must be a single character.")
+    exit()
+
+if typed_word[0] == typed_character:
+    print(typed_character + " found at index 0")
+    number = number + 1
+if typed_word[1] == typed_character: 
+    print(typed_character + " found at index 1")
+    number = number + 1
+if typed_word[2] == typed_character:
+    print(typed_character + " found in index 2")
+    number = number + 1
+if typed_word[3] == typed_character:
+    print(typed_character + " found in index 3")
+    number = number + 1
+if typed_word[4] == typed_character:
+    print(typed_character + " found in index 4")
+    number = number + 1
+
+if number == 0:
+    print("No instances of " + typed_character + " found in " + typed_word)
+else: 
+    if number == 1: 
+        print("1 instance of " + typed_character + " found in " + typed_word)
+    else:
+        if number == 2: 
+            print("2 instances of " + typed_character + " found in " + typed_word)
+        else:
+            if number == 3: 
+                print("3 instance of " + typed_character + " found in " + typed_word)
+            else:
+                if number == 4: 
+                    print("3 instance of " + typed_character + " found in " + typed_word)
+                else: 
+                    if number == 5: 
+                        print("5 instance of " + typed_character + " found in " + typed_word)
