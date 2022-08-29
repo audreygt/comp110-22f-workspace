43ca40d0c7b5a404089d71ee61085609bf017015
audreygt <audreyt@unc.edu>
2022-08-29 13:15:38 -0400

    getting my life together

--- lessons/input.py
+++ lessons/input.py
@@ -3,6 +3,3 @@
 user_name: str = input("What is your name?" )
 print("Hello, " + user_name + ", good morning!")
 print("You are awesome, " + user_name)
-print(user_name + ", you need to put your teeth up.")
-print(user_name + ", your family is full of clowns.")
-print("You go to sleep too early, " + user_name)
