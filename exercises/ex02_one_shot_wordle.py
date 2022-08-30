68b60c2e982728a87f91e6e200abdefefbce5321
audreygt <audreyt@unc.edu>
2022-08-30 17:01:08 -0400

    i figured it out

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -3,16 +3,17 @@
 __author__ = "730545277"
 
 secret_word: str = "python"
-guessed_word: str = input("What is your 6-letter guess?")
-character_number: len(secret_word)
+character_number = len(secret_word)
+guessed_word: str = input("What is your 6-letter guess? ")
+
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
-while len(guessed_word)!= character_number:
-    print(f"That was not 6 letters! Try again: {guessed_word}")
-    print("Not quite. Play again soon!")
-    character_number = len(secret_word)
 
-while len(guessed_word) == character_number:
-    if guessed_word == secret_word:
-        print("Woo! You got it!")
+while len(guessed_word) != character_number:
+    guessed_word = input("That was not 6 letters! Try again: ")
+    
+
+
+
+   
