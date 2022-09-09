d7a343bf27974e7367968671a55c1ae48f900db2
audreygt <audreyt@unc.edu>
2022-09-09 12:28:45 -0400

    slowly making it

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -20,24 +20,23 @@ WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 index: int = 0
+checking: int = 0
 right_character: bool = False
+emoji_answer: str = ""
 
-def emojified(guessed_word: str, typed_word: str) -> str:
+def emojified(guessed_word: str, secret_word: str) -> str:
     """testing for the indexes using colored boxes"""
-    assert len(guessed_word) == len(typed_word)
-    while len(guessed_word) == len(typed_word):
-        if guessed_word[index] == typed_word[index]:
-            return GREEN_BOX
-    if guessed_word[index] != typed_word[index]:
-        while right_character is not True and checking < len(guessed_word):
-            if typed_word[checking] == guessed_word[index]:
-                right_character = True 
+    assert len(guessed_word) == len(secret_word)
+    contains_char(guessed_word, secret_word)
+    while contains_char is True:
+        emoji_answer += GREEN_BOX
+    while contains_char is False and checking < len(guessed_word):
+        if secret_word[checking] == guessed_word[index]:
+            right_character = True 
             checking += 1
         if right_character is not True:
-            return WHITE_BOX
+            emoji_answer += WHITE_BOX
         else:
-            return YELLOW_BOX
+            emoji_answer += YELLOW_BOX
+    return emoji_answer
 
-def input_guess(n: int) -> str:
-    """allowing user multiples tried until correct length"""
-    if 
