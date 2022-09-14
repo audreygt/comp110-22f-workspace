f362d4f9cb6fc8b6cabd3f1d1d9d5929736e85d0
audreygt <audreyt@unc.edu>
2022-09-14 08:50:08 -0400

    starting from the bottmo

---
+++ exercises/ex04_utils.py
@@ -0,0 +1,10 @@
+"""Coding functions and using lists."""
+
+__author__ = "730545277"
+
+from random import randint 
+
+def all(sequence: list(), int: int) -> bool:
+    number_set: list()
+    check_number: int = randint(1,10)
+    while 
