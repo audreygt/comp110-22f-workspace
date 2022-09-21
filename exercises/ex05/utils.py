892f5b908c76d2446e36381df478fa32d95625a5
audreygt <audreyt@unc.edu>
2022-09-21 18:39:15 -0400

    getting it

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -7,10 +7,10 @@ def only_evens(evens: list[int]) -> list[int]:
     """Returns a list of even integers given a list of integers."""
     even_list: list[int] = list()
     index: int = 0
-    while index <= len(evens):
+    while index < len(evens):
         if evens[index] % 2 == 0:
-            even_list += evens[index]
-        index +=1
+            even_list.append(evens[index])
+        index += 1
     return even_list
 
 
@@ -18,21 +18,29 @@ def concat(list_1: list[int], list_2: list[int]) -> list[int]:
     """Returns all integers from first and second lists in one."""
     all_list: list[int] = list()
     index: int = 0
-    while index <= list_1:
-        all_list += list_1[index]
+    while index < len(list_1):
+        all_list.append(list_1[index])
         index += 1 
     
     index = 0
-    while index <= list_2:
-        all_list += list_2[index]
+    while index < len(list_2):
+        all_list.append(list_2[index])
         index += 1
     return all_list 
 
 
 def sub(list: list[int], start_index: int, end_index: int) -> list[int]:
     """Returns a list of integers between given indexes."""
-    subset_list: list[int] = list()
-    while start_index <= end_index:
-        subset_list += list[start_index]
-        start_index +=1 
+    subset_list: list[int] = ()
+    index: int = start_index + 1
+    
+    if start_index > len(list):
+        return []
+    if len(list) == 0:
+        return []
+    if end_index < 0:
+        return []
+    while list[0] < start_index < len(list) and list[0] < end_index < len(list):
+        subset_list.append(list[index])
+        start_index += 1
     return subset_list
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,3 +1,30 @@
 """Testing different functions."""
 
 __author__ = "730545277"
+
+
+from utils import only_evens
+from utils import sub
+from utils import concat 
+
+
+def test_only_evens() -> None:
+    """Testing only evens."""
+    evens: list[int] = list[2, 3, 4, 5]
+    assert only_evens(evens) == [2, 4]
+
+
+def test_sub() -> None:
+    """Testing subs."""
+    list: list[int] = [10, 20, 30, 40]
+    start_index: int = 1
+    end_index: int = 3
+    assert start_index <= end_index
+    assert sub(list, start_index, end_index) == [20, 30]
+
+
+def test_concat() -> None: 
+    """Testing concat."""
+    list_1: list[int] = [0, 1, 2]
+    list_2: list[int] = [3, 4, 5]
+    assert concat(list_1, list_2) == [0, 1, 2, 3, 4, 5]
