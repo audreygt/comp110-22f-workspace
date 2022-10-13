548563624e966f6154c7ad9ab531d83048eb908f
audreygt <audreyt@unc.edu>
2022-10-13 16:15:34 -0400

    ok

--- exercises/ex08/data_utils.py
+++ exercises/ex08/data_utils.py
@@ -6,7 +6,17 @@ __author__ = "730545277"
 from csv import DictReader 
 
 
-def read_csv_rows(filename:str) -> list[dict[str, str]]:
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
     """Read the rows of a CSV into a table (list of dicts)."""
     result: list[dict[str, str]] = []
+    file_handle = open(filename, "r", encoding="utf8")
+    csv_reader = DictReader(file_handle)
+
+    return result 
+
+
+def column_values(tablerows: list[dict[str, str]], columnname: str) -> list[str]:
+    result: list[str] = list()
+    for row in tablerows:
+        result.append(row[columnname])
     return result 
