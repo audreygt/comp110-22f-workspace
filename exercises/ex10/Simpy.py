dea2466c9d6ac6fda618e243e27988e0a24f78d4
audreygt <audreyt@unc.edu>
2022-11-14 12:56:33 -0500

    wow

--- exercises/ex10/Simpy.py
+++ exercises/ex10/Simpy.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 from typing import Union
 
-__author__ = "YOUR PID HERE"
+__author__ = "730545277"
 
 
 class Simpy:
@@ -25,10 +25,10 @@ class Simpy:
 
     def arange(self, start: float, stop: float, step: float = 1.0) -> None: 
         """Implementing numbers throuhg a range skipping by step."""
-        stop -= step
-        floats: float = start
-        while floats <= stop: 
-            self.values.append(floats + step)
+        ending: float = stop - step
+        while abs(start) <= abs(ending):
+            self.values.append(start)
+            start += step
 
     def sum(self) -> float:
         """Sums up all values in the Simpy list."""
@@ -36,28 +36,28 @@ class Simpy:
         return total 
 
     def __add__(self, choice: Union[Simpy, float]) -> Simpy:
-        result: Simpy = Simpy([])
+        result: list[float] = []
         if isinstance(choice, Simpy):
             assert len(self.values) == len(choice.values)
             for i in range(len(self.values)):
-                result += (self.values[i] + choice.values[i])
+                result.append(self.values[i] + choice.values[i])
             
         if isinstance(choice, float):
             for i in range(len(self.values)):
-                result += (self.values[i] + choice)
-        return result
+                result.append(self.values[i] + choice)
+        return Simpy(result)
 
     def __pow__(self, choice: Union[Simpy, float]) -> Simpy:
-        result: Simpy = Simpy([])
+        result: list[float] = []
         if isinstance(choice, Simpy):
             assert len(self.values) == len(choice.values)
             for i in range(len(self.values)):
-                result += (self.values[i] ** choice.values[i])
+                result.append(self.values[i] ** choice.values[i])
             
         if isinstance(choice, float):
             for i in range(len(self.values)):
-                result += (self.values[i] ** choice)
-        return result
+                result.append(self.values[i] ** choice)
+        return Simpy(result)
 
     def __eq__(self, choice: Union[Simpy, float]) -> list[bool]:
         result: list[bool] = []
@@ -66,7 +66,7 @@ class Simpy:
             for i in range(len(self.values)):
                 if self.values[i] == choice.values[i]:
                     result.append(True)
-                else: 
+                else:
                     result.append(False)
         if isinstance(choice, float):
             for i in range(len(self.values)):
@@ -83,7 +83,7 @@ class Simpy:
             for i in range(len(self.values)):
                 if self.values[i] > choice.values[i]:
                     result.append(True)
-                else: 
+                elif self.values[i] < choice.values[i]:
                     result.append(False)
         if isinstance(choice, float):
             for i in range(len(self.values)):
@@ -93,28 +93,15 @@ class Simpy:
                     result.append(False)
         return result 
 
-    def __getititem__(self, choice: Union[int, list[bool]]) -> float:
-        result: float = 0.0
+    def __getititem__(self, choice: Union[int, list[bool]]) -> Union[float, Simpy]:
         if isinstance(choice, int):
-            if choice < len(self.values):
-                result = self.values[choice]
-        if isinstance(choice, list[bool]):
+            numbers: list[float] = list(self.values)
+            if choice < len(numbers):
+                return(numbers[choice])
+        else:
+            result: list[float] = []
             for i in choice:
                 if self.values[i] == choice[True]:
-                    result += (self.values[i])
-        return result 
-        
-      
-        
-
-
-
-
-
-            
-    
-
-
-
-
-        
+                    result.append(self.values[i])
+                    return Simpy(result)
+       
