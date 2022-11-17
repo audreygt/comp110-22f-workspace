0734fb63f3283e6e09da8e959d9efdad00fbdcab
audreygt <audreyt@unc.edu>
2022-11-16 20:56:01 -0500

    yeet

--- exercises/ex10/Simpy.py
+++ exercises/ex10/Simpy.py
@@ -7,12 +7,15 @@ __author__ = "730545277"
 
 
 class Simpy:
+    """Simping for simpy."""
     values: list[float]
 
     def __init__(self, values: list[float]):
+        """Constructing a list of values."""
         self.values = values 
 
     def __repr__(self) -> str:
+        """Constructing a string."""
         return f"Simpy({self.values})"
 
     def fill(self, floats: float, number: int) -> None: 
@@ -36,6 +39,7 @@ class Simpy:
         return total 
 
     def __add__(self, choice: Union[Simpy, float]) -> Simpy:
+        """Adding values together."""
         result: list[float] = []
         if isinstance(choice, Simpy):
             assert len(self.values) == len(choice.values)
@@ -48,6 +52,7 @@ class Simpy:
         return Simpy(result)
 
     def __pow__(self, choice: Union[Simpy, float]) -> Simpy:
+        """Using exponents and increasing values."""
         result: list[float] = []
         if isinstance(choice, Simpy):
             assert len(self.values) == len(choice.values)
@@ -60,6 +65,7 @@ class Simpy:
         return Simpy(result)
 
     def __eq__(self, choice: Union[Simpy, float]) -> list[bool]:
+        """Checking for anything that equals."""
         result: list[bool] = []
         if isinstance(choice, Simpy):
             assert len(self.values) == len(choice.values)
@@ -77,9 +83,9 @@ class Simpy:
         return result 
 
     def __gt__(self, choice: Union[Simpy, float]) -> list[bool]:
+        """Returning bools based on conditions."""
         result: list[bool] = []
         if isinstance(choice, Simpy):
-            assert len(self.values) == len(choice.values)
             for i in range(len(self.values)):
                 if self.values[i] > choice.values[i]:
                     result.append(True)
@@ -94,13 +100,13 @@ class Simpy:
         return result 
 
     def __getitem__(self, choice: Union[int, list[bool]]) -> Union[float, Simpy]:
+        """Pulling items out using bools."""
         if isinstance(choice, int):
-            if choice < len(self.values):
-                return (self.values[choice])
+            return (self.values[choice])
             
-        elif isinstance(choice, Simpy): 
+        else: 
             result: list[float] = []
-            for i in choice:
-                if choice[i] == True:
+            for i in range(len(choice)):
+                if choice[i] is True:
                     result.append(self.values[i])
-            return Simpy(result)
+            return Simpy(result)    
---
+++ lessons/review.py
@@ -0,0 +1,86 @@
+class Student:
+    email: str 
+
+    def __init__(self, email):
+        self.email = email 
+
+class Gradebook:
+    grades: dict[Student, float]
+
+    def __init__(self):
+        self.grades = {}
+
+    def add_student(self, student: Student, grade: float) -> None:
+        self.grades[student] = grade 
+    
+    def outreach(self) -> list[str]:
+        result: list[str] = []
+        for student in self.grades:
+            if self.grades[student] < 75.0:
+                result.append(student.email)
+        return result 
+
+class ChristmasTreeFarm:
+    plots: list[int]
+
+    def __init__(self, plots: int, initial_planting: int):
+        self.plots = []
+        self.plots.append(initial_planting)
+        for plots in range(plots):
+            self.plots.append(0)
+
+    def plant(self, integer: int):
+        self.plots[integer] = 1
+
+    def growth(self):
+        for i in range(len(self.plots) - 1):
+            if self.plots[i] > 0:
+                self.plots[i] += 1
+
+    def harvest(self, replant: bool) -> int:
+        harvested: int = 0 
+        for i in range(len(self.plots) - 1): 
+            if self.plots[i] >= 5: 
+                if  replant:
+                    self.plots[i] = 1
+                elif not replant:
+                    self.plots[i] = 0
+                harvested += 1
+        return harvested
+
+    def __add__(self, another: ChristmasTreeFarm) -> ChristmasTreeFarm:
+        new_farm: int = 0 
+        for i in self.plots: 
+            if i > 0:
+                new_farm += 1
+        for i in another:
+            if i > 0:
+                new_farm += 1
+        return ChristmasTreeFarm(new_farm)
+
+class Course: 
+        name: str
+        level: int 
+        prerequisites: list[str]
+
+    def __init__(self, names, levels, prerequisites):
+        self.name = names
+        self.level = levels
+        self.prerequistes = prerequisites 
+
+    
+    def find_courses(list_courses: list[Courses], prereq: list[str]) -> list[str]: 
+        advanced_classes: list[str] = []
+        for i in list_courses:
+            if i.level >= 400: 
+                for requirements in i.prerequisites:
+                    if requirements == prereq:
+                        advanced_classes.append(i.name)
+        return advanced_classes 
+
+    def is_valid_course(self, prereq: list[str]) -> bool:
+        if self.level >= 400 and self.prerequisites == prereq:
+            return True 
+        return False 
+            
+
